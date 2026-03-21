f0876ef4e0b8f38cf274022670fe33e00e71702a
lamtung16 <nguyenlamtung10@gmail.com>
2026-03-20 18:03:12 -0700

    update logo

--- resolution.py
+++ resolution.py
@@ -4,6 +4,7 @@ from PIL import Image
 # Folder containing PNG files (change if needed)
 folder_path = "logos"
 
+size = 150
 for filename in os.listdir(folder_path):
     if filename.lower().endswith(".png"):
         file_path = os.path.join(folder_path, filename)
@@ -11,11 +12,10 @@ for filename in os.listdir(folder_path):
         with Image.open(file_path) as img:
             width, height = img.size
             
-            # Determine scaling factor so smaller side becomes 50 px
             if width < height:
-                scale = 50 / width
+                scale = size / width
             else:
-                scale = 50 / height
+                scale = size / height
             
             new_width = int(width * scale)
             new_height = int(height * scale)
