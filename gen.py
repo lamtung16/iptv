26b4f17b6bf5f4ae2909553fe4300910d5a00e7d
lamtung16 <nguyenlamtung10@gmail.com>
2026-06-28 12:18:20 -0700

    concise

--- gen.py
+++ gen.py
@@ -2,7 +2,7 @@ import csv
 import re
 
 # source list
-source_list = ["kstv","backup","backup2","proxpanel","vuzemedia"]
+source_list = ["kstv","backup","proxpanel","vuzemedia"]
 
 def parse_id_list(s):
     if not s or s.strip() == "[]":
